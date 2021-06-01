ebd77ae0b0ccdb54bbdc422ca929ca72be7c9a95
Theo <theo.poette@epitech.eu>
2021-06-01 16:46:35 +0200

    [+] boilerplate for front application

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,26 @@
+import PostMessage from '../models/postMessage.js';
 
-export const getPosts = (req, res) => {
-    res.send('dis works');
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        console.log(postMessages);
+        res.status(200).json(postMessages);
+    } catch (err) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (err) {
+        res.status(409).json({ message: err.message });
+    }
+    res.send('Post Creation');
 }
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,3 +1,12 @@
 import mongoose from 'mongoose';
 
-const postSchema = mongoose.Schema({})
+const postSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    tags: [String],
+    selectedFile: String,
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
