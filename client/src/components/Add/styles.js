75cf981c6d7c179b994aec30381cf97f41febbd9
Theo <theo.poette@epitech.eu>
2021-07-09 13:53:33 +0200

    [+] add button

--- client/src/components/Add/styles.js
+++ client/src/components/Add/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles((theme) => ({
+export default makeStyles(() => ({
     button: {
         bottom: '50px',
         position: 'sticky',
