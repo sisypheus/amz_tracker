34947d887d96cd28f8a2e1f9b9049da305aba442
Theo <theo.poette@epitech.eu>
2021-07-16 16:44:12 +0200

    fix: button styling

--- client/src/components/Add/styles.js
+++ client/src/components/Add/styles.js
@@ -5,14 +5,13 @@ export default makeStyles(() => ({
         bottom: '50px',
         position: 'sticky',
         margin: '0 auto',
-        display: 'flex',
         borderRadius: '15px 15px 15px 15px',
-        background: '#FF9900',
+        background: '#FF9900 !important',
         fontWeight: 'bold',
         padding: '7px 40px',
         lineHeight: '2.5',
         '&:hover': {
-            background: '#cb7a00',
+            background: '#cb7a00 !important',
         },
     },
     buttonWrapper: {
