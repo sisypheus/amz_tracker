298e1206d9c924922859299af86147be5535f980
Theo <theo.poette@epitech.eu>
2021-07-16 21:05:55 +0200

    fix: setting user agent for webview

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -117,6 +117,7 @@ const Auth = () => {
               )}
               onSuccess={googleSuccess}
               onFailure={googleFailure}
+              userAgent="Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
             />
             <Grid container justify="flex-end">
                 <Grid item>
