4f8e51d8adb8570758f8ba51686bf5a2b9229bed
Theo <theo.poette@epitech.eu>
2021-07-16 16:51:03 +0200

    fix: material ui build styling

--- client/src/components/Add/Add.js
+++ client/src/components/Add/Add.js
@@ -4,7 +4,7 @@ import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
 const Add = () => {
-    const classes = useStyles();
+    const classes = useStyles({index: 1});
 
     return (
         <div className={classes.buttonWrapper}>
